1194f641ab453e2e25cef01eea0b2f2cb7a165a7
Alex Oakley <alex@alexoakley.com>
2023-01-16 01:48:40 -0800

    Configured publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
-    `maven-publish`
     id("org.jetbrains.dokka")
+    id("io.github.gradle-nexus.publish-plugin")
+    `maven-publish`
+    signing
 }
 
 group = "dev.sublab"
@@ -32,6 +34,7 @@ val sublabEncryptingVersion: String by project
 dependencies {
     testImplementation(kotlin("test"))
     dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:$dokkaVersion")
+    dokkaJavadocPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:$dokkaVersion")
 
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
@@ -78,11 +81,71 @@ val sourcesJar by tasks.registering(Jar::class) {
     from(sourceSets.main.get().allSource)
 }
 
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    dependsOn("dokkaJavadoc")
+    from("$buildDir/dokka/javadoc")
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
 publishing {
     publications {
         register("mavenJava", MavenPublication::class) {
+            groupId = groupId
+            artifactId = rootProject.name
+            version = version
+
             from(components["java"])
             artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+
+            pom {
+                name.set(rootProject.name)
+                description.set("Sublab's Common Kotlin library")
+                url.set("https://github.com/sublabdev/${rootProject.name}")
+                licenses {
+                    license {
+                        name.set("Apache 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Sublab")
+                        email.set("info@sublab.dev")
+                        organization.set("Substrate Laboratory LLC")
+                        organizationUrl.set("https://sublab.dev")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/sublabdev/${rootProject.name}.git")
+                    developerConnection.set("scm:git:https://github.com/sublabdev/${rootProject.name}.git")
+                    url.set("https://github.com/sublabdev/${rootProject.name}")
+                }
+            }
         }
     }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            packageGroup.set("dev.sublab")
+            username.set(extra.get("ossrhUsername") as? String)
+            password.set(extra.get("ossrhPassword") as? String)
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
